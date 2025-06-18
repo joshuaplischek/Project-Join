6e41f7d35c318ea6376240d4d1f1cde25fca3850
Anne Dalchow <anne.dalchow@web.de>
2025-06-18 13:14:02 +0200

    init firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"join-7e54b","appId":"1:424943017502:web:0a1d69d4e4a85c3569580f","storageBucket":"join-7e54b.firebasestorage.app","apiKey":"AIzaSyCnn-zzaGhj-Hh20QYqwumfSFlpFGNIyxs","authDomain":"join-7e54b.firebaseapp.com","messagingSenderId":"424943017502"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
