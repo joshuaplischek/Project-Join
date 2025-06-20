981e296a542f426b4097b00c11d0461018884734
Anne Dalchow <anne.dalchow@web.de>
2025-06-20 09:26:21 +0200

    move nav, header to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrivacypoliceComponent } from "./pages/privacypolice/privacypolice.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { NavComponent } from "./shared/nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrivacypoliceComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
