0da99289db7979eb6c31268b3623144bf9f41b54
Anne Dalchow <anne.dalchow@web.de>
2025-06-20 09:27:38 +0200

    delete unused components

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../shared/header/header.component";
-import { NavComponent } from "../shared/nav/nav.component";
-import { QuickMenuComponent } from "../shared/quick-menu/quick-menu.component";
 import { FirebaseService } from '../shared/services/firebase.service';
-import { Contactlist } from '../contactlist';
 import { ContactsComponent } from "./contacts/contacts.component";
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [HeaderComponent, NavComponent, QuickMenuComponent, ContactsComponent],
+  imports: [ContactsComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
