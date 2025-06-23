1e2c5f78506d514692c848d494461e69881da1b9
Anne Dalchow <anne.dalchow@web.de>
2025-06-23 16:49:42 +0200

    add routerlink and active class for clicked elements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { LegalnoticeComponent } from './pages/legalnotice/legalnotice.component';
 import { PrivacypoliceComponent } from './pages/privacypolice/privacypolice.component';
+import { ContactsComponent } from './mainpage/contacts/contacts.component';
 
 export const routes: Routes = [
  { path: '', component: MainpageComponent },
+ { path: 'contacts', component: ContactsComponent },
  { path: 'legalnotice', component: LegalnoticeComponent },
  { path: 'privacypolicy', component: PrivacypoliceComponent }
 ];
