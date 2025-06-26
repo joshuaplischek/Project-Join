73f209501ac6f845e318a6ec35b5c635b0fe81c6
Anne Dalchow <anne.dalchow@web.de>
2025-06-26 13:03:19 +0200

    add board, addTask component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { LegalnoticeComponent } from './pages/legalnotice/legalnotice.component'
 import { PrivacypoliceComponent } from './pages/privacypolice/privacypolice.component';
 import { ContactsComponent } from './mainpage/contacts/contacts.component';
 import { HelpComponent } from './pages/help/help.component';
+import { BoardComponent } from './mainpage/board/board.component';
+import { AddTaskComponent } from './mainpage/add-task/add-task.component';
 
 export const routes: Routes = [
  { path: '', component: MainpageComponent },
  { path: 'contacts', component: ContactsComponent },
+ { path: 'board', component: BoardComponent },
+ { path: 'add-task', component: AddTaskComponent },
  { path: 'legalnotice', component: LegalnoticeComponent },
  { path: 'privacypolicy', component: PrivacypoliceComponent },
- { path: 'help', component: HelpComponent}
+ { path: 'help', component: HelpComponent }
 ];
--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { FirebaseService } from '../shared/services/firebase.service';
 import { ContactsComponent } from "./contacts/contacts.component";
+import { BoardComponent } from "./board/board.component";
+import { AddTaskComponent } from "./add-task/add-task.component";
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [ContactsComponent],
+  imports: [ContactsComponent, BoardComponent, AddTaskComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
 export class MainpageComponent {
 
-
-
-
 }
+
+
