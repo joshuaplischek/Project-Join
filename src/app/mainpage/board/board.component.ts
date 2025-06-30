2b1b6d3078302bfdade4e58b3eaefd7bff92ed06
Joshua Plischek <plischekjoshua@web.de>
2025-06-30 13:44:10 +0200

    Add drag and drop function

--- src/app/mainpage/board/board.component.ts
+++ src/app/mainpage/board/board.component.ts
@@ -1,40 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {CdkDrag, CdkDragDrop, CdkDropList, DragDropModule, moveItemInArray, transferArrayItem,} from '@angular/cdk/drag-drop';
 import { CompactTaskComponent } from '../../shared/compact-task/compact-task.component';
 import { TasksFirbaseService } from '../../shared/services/tasks-firbase.service';
+import { Tasks } from '../../../interfaces/tasks';
+import { doc, updateDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [CompactTaskComponent],
+  imports: [CompactTaskComponent, DragDropModule, CdkDrag, CdkDropList],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BoardComponent {
-  constructor(public taskService: TasksFirbaseService) { }
+  constructor(public taskService: TasksFirbaseService) {}
+  @Input() selcetedTask: Tasks | null = null;
 
-  dragElement: string | undefined;
+  todo: Tasks[] = [];
+
+  inprogress: Tasks[] = [];
+
+  awaitfeedback: Tasks[] = [];
+
+  done: Tasks[] = [];
 
   get todoTasks() {
-    return this.taskService.tasks.filter(task => task.status === 'todo');
+    this.todo = this.taskService.tasks.filter((task) => task.status === 'todo');
+    return this.todo;
   }
 
   get inProgressTasks() {
-    return this.taskService.tasks.filter(task => task.status === 'inprogress');
+    this.inprogress = this.taskService.tasks.filter(
+      (task) => task.status === 'inprogress'
+    );
+    return this.inprogress;
   }
 
   get awaitFeedbackTasks() {
-    return this.taskService.tasks.filter(task => task.status === 'awaitfeedback');
+    this.awaitfeedback = this.taskService.tasks.filter(
+      (task) => task.status === 'awaitfeedback'
+    );
+    return this.awaitfeedback;
   }
 
   get doneTasks() {
-    return this.taskService.tasks.filter(task => task.status === 'done');
+    this.done = this.taskService.tasks.filter((task) => task.status === 'done');
+    return this.done;
   }
 
-  startDragging(taskId: any) {
-   this.dragElement = taskId;
+  drop(event: CdkDragDrop<Tasks[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
 
+    const movedTask = event.container.data[event.currentIndex];
+    const newStatus = this.getStatusByArray(event);
+    if (movedTask && newStatus) {
+      movedTask.status = newStatus;
+      if (typeof movedTask.id === 'string') {
+        this.taskService.updateTaskStatus(movedTask.id, { status: newStatus });
+      }
+    }
+    }
   }
 
-
-
+private getStatusByArray(event: CdkDragDrop<Tasks[]>) {
+  const id = (event.container.id as string);
+  if (id.includes('todo')) return 'todo';
+  if (id.includes('inprogress')) return 'inprogress';
+  if (id.includes('awaitfeedback')) return 'awaitfeedback';
+  if (id.includes('done')) return 'done';
+  return '';
+}
 }
