8c084c20705304948a78a07c9861b564afc2ee01
MirkanPolat <Mirkanpolat@outlook.de>
2025-07-06 17:07:03 +0200

    adding the icon color problem and the progress bar

--- src/app/shared/compact-task/compact-task.component.ts
+++ src/app/shared/compact-task/compact-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksFirbaseService } from '../services/tasks-firbase.service';
+import { FirebaseService } from '../services/firebase.service';
 import { Subtask, Tasks } from '../../../interfaces/tasks';
 
 @Component({
@@ -12,7 +13,10 @@ import { Subtask, Tasks } from '../../../interfaces/tasks';
 })
 export class CompactTaskComponent {
 
-  constructor(private taskService: TasksFirbaseService) { }
+  constructor(
+    private taskService: TasksFirbaseService,
+    private contactService: FirebaseService
+  ) { }
 
   @Input() task!: Tasks;
   @Output() taskClick = new EventEmitter<Tasks>();
@@ -57,4 +61,23 @@ export class CompactTaskComponent {
     }
   }
 
+  getColorForLetter(letter: string): string {
+    return this.contactService.getColorForLetter(letter);
+  }
+
+  get assignedUsers(): string[] {
+    return this.task?.assignedTo || [];
+  }
+
+  get hasAssignedUsers(): boolean {
+    return this.assignedUsers.length > 0;
+  }
+
+  getDisplayedUsers(): string[] {
+    return this.assignedUsers.slice(0, 3);
+  }
+
+  getRemainingUsersCount(): number {
+    return Math.max(0, this.assignedUsers.length - 3);
+  }
 }
