dadc3eb482ffc455b398f1a532246afbcc7fbb97
anne-dalchow <151674538+anne-dalchow@users.noreply.github.com>
2025-07-07 09:11:44 +0200

    add angular material lib

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,5 +8,5 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"join-7e54b","appId":"1:424943017502:web:0a1d69d4e4a85c3569580f","storageBucket":"join-7e54b.firebasestorage.app","apiKey":"AIzaSyCnn-zzaGhj-Hh20QYqwumfSFlpFGNIyxs","authDomain":"join-7e54b.firebaseapp.com","messagingSenderId":"424943017502"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"join-7e54b","appId":"1:424943017502:web:0a1d69d4e4a85c3569580f","storageBucket":"join-7e54b.firebasestorage.app","apiKey":"AIzaSyCnn-zzaGhj-Hh20QYqwumfSFlpFGNIyxs","authDomain":"join-7e54b.firebaseapp.com","messagingSenderId":"424943017502"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideAnimationsAsync(), provideAnimationsAsync()]
 };
