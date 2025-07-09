d2b998b932bebf8b5727884f9277f9e59ce848c0
Joshua Plischek <plischekjoshua@web.de>
2025-07-09 10:59:25 +0200

    change paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,9 @@ import { ContactsComponent } from './mainpage/contacts/contacts.component';
 import { HelpComponent } from './pages/help/help.component';
 import { BoardComponent } from './mainpage/board/board.component';
 import { AddTaskComponent } from './mainpage/add-task/add-task.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
- { path: '', component: AppComponent },
+ { path: '', component: BoardComponent },
  { path: 'contacts', component: ContactsComponent },
  { path: 'board', component: BoardComponent },
  { path: 'add-task', component: AddTaskComponent },
