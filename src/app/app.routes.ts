65b2ab1e2701e236cbabd9f9deeab392c5f86a12
Joshua Plischek <plischekjoshua@web.de>
2025-07-09 10:31:01 +0200

    change main path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
-import { MainpageComponent } from './mainpage/mainpage.component';
 import { LegalnoticeComponent } from './pages/legalnotice/legalnotice.component';
 import { PrivacypoliceComponent } from './pages/privacypolice/privacypolice.component';
 import { ContactsComponent } from './mainpage/contacts/contacts.component';
 import { HelpComponent } from './pages/help/help.component';
 import { BoardComponent } from './mainpage/board/board.component';
 import { AddTaskComponent } from './mainpage/add-task/add-task.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
- { path: '', component: MainpageComponent },
+ { path: '', component: AppComponent },
  { path: 'contacts', component: ContactsComponent },
  { path: 'board', component: BoardComponent },
  { path: 'add-task', component: AddTaskComponent },
