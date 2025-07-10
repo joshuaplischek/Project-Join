c2c230ae006e266c3f68d36f7d010768cb79565d
anne-dalchow <151674538+anne-dalchow@users.noreply.github.com>
2025-07-10 08:56:57 +0200

    feat(routes): add redirectTo and pathMatch to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,19 +6,23 @@ import { HelpComponent } from './pages/help/help.component';
 import { BoardComponent } from './mainpage/board/board.component';
 import { AddTaskComponent } from './mainpage/add-task/add-task.component';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
+// localhost4200/ URL Pfad ist '' -> exakte Übereinstimmung -> Weiterleitung zu /login
+// localhost4200/board URL Pfad ist 'board' -> keine Übereinstimmung -> keine Weiterleitung
 export const routes: Routes = [
- { path: '', component: BoardComponent },
- { path: 'contacts', component: ContactsComponent },
- { path: 'board', component: BoardComponent },
- { path: 'add-task', component: AddTaskComponent },
- { path: 'legalnotice', component: LegalnoticeComponent },
- { path: 'privacypolicy', component: PrivacypoliceComponent },
- { path: 'help', component: HelpComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'board', component: BoardComponent },
+  { path: 'add-task', component: AddTaskComponent },
+  { path: 'legalnotice', component: LegalnoticeComponent },
+  { path: 'privacypolicy', component: PrivacypoliceComponent },
+  { path: 'help', component: HelpComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true })],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
