3b2a08f3d96b291ed0bb5b6c526a03c2a16f7ac1
anne-dalchow <151674538+anne-dalchow@users.noreply.github.com>
2025-07-10 08:59:41 +0200

    Update mainpage.component.ts

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { FirebaseService } from '../shared/services/firebase.service';
-
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
   imports: [],
   templateUrl: './mainpage.component.html',
-  styleUrl: './mainpage.component.scss'
+  styleUrl: './mainpage.component.scss',
 })
-export class MainpageComponent {
-
-}
-
-
+export class MainpageComponent {}
