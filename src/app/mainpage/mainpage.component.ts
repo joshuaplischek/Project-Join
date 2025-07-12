5addb286fd2f27e9dcbe957e4c86904e0bb9553e
Joshua Plischek <plischekjoshua@web.de>
2025-07-12 11:06:31 +0200

    add greeting

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { TasksFirbaseService } from '../shared/services/tasks-firbase.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss',
 })
 export class MainpageComponent {
   constructor(public taskService: TasksFirbaseService) {}
-  
-ngOnInit() {
-  this.taskService.tasksChanged.subscribe(() => {
-    console.log('Tasks geladen:', this.taskService.tasks);
-  });
-  
-}
+
+  ngOnInit() {
+    this.taskService.tasksChanged.subscribe(() => {
+      console.log('Tasks geladen:', this.taskService.tasks);
+    });
+  }
+
+  getGreeting() {
+    let dateTime = new Date();
+    let hour = dateTime.getHours();
+    if (hour < 12) return 'Good morning';
+    if (hour < 18) return 'Good afternoon';
+    return 'Good evening';
+  }
 }
