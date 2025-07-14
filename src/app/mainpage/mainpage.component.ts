0b395f61ffc91674e5269fc4e028d2b2a2189fb3
Joshua Plischek <plischekjoshua@web.de>
2025-07-14 14:58:40 +0200

    edit respo summary

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -23,9 +23,9 @@ export class MainpageComponent {
   getGreeting() {
     let dateTime = new Date();
     let hour = dateTime.getHours();
-    if (hour < 12) return 'Good morning';
-    if (hour < 18) return 'Good afternoon';
-    return 'Good evening';
+    if (hour < 12) return 'Good morning,';
+    if (hour < 18) return 'Good afternoon,';
+    return 'Good evening,';
   }
 
   getNumberOftasks(task: string) {
