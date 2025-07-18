59b6eb135af0099ad5193a4b00cdfc53a2a76332
Anne Dalchow <anne.dalchow@web.de>
2025-07-18 11:29:02 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LegalnoticeComponent } from './pages/legalnotice/legalnotice.component'
 import { PrivacypoliceComponent } from './pages/privacypolice/privacypolice.component';
 import { ContactsComponent } from './mainpage/contacts/contacts.component';
 import { HelpComponent } from './pages/help/help.component';
-import { BoardComponent } from './mainpage/board/board.component';
 import { AddTaskComponent } from './mainpage/add-task/add-task.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
@@ -15,7 +14,12 @@ export const routes: Routes = [
   { path: 'summary', component: MainpageComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'contacts', component: ContactsComponent, canActivate: [AuthGuard] },
-  { path: 'board', canActivate: [AuthGuard], loadComponent: () => import('./mainpage/board/board.component').then(m => m.BoardComponent) },
+  {
+    path: 'board',
+    canActivate: [AuthGuard],
+    loadComponent: () =>
+      import('./mainpage/board/board.component').then((m) => m.BoardComponent),
+  },
   { path: 'add-task', component: AddTaskComponent, canActivate: [AuthGuard] },
   { path: 'legalnotice', component: LegalnoticeComponent },
   { path: 'privacypolicy', component: PrivacypoliceComponent },
